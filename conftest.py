0ea674241ac87b47ee8a4c1b4e5a1d3c6ccfd457
creamberry <creamberry@ya.ru>
2024-08-23 18:51:32 +0300

    browser choosing option added

--- conftest.py
+++ conftest.py
@@ -1,19 +1,45 @@
 from selenium import webdriver
 from selenium.webdriver.chrome.options import Options as Chrome_options
+from selenium.webdriver.firefox.options import Options as Firefox_options
+from selenium.webdriver.firefox.firefox_profile import FirefoxProfile
 import pytest
+
 
 def pytest_addoption(parser):
     parser.addoption("--language", action="store", default="ru")
+    parser.addoption("--browser", action="store", default="chrome")
+
 
 @pytest.fixture(scope="function")
-def browser(request):
-    lang = request.config.getoption("--language")
-    options = Chrome_options()
+def language(request):
+    """Определяем возможность выбора языка"""
+    lang =  request.config.getoption("--language")
     if lang in ["en", "fr", "de", "es", "ru"]:
-        options.add_argument("lang="+lang)
+        return lang
     else:
         raise ValueError("Invalid language")
 
-    driver = webdriver.Chrome(options=options)
+
+@pytest.fixture(scope="function")
+def browser(request, language):
+    """Определяем возможность выбора браузера"""
+    if request.config.getoption("--browser") == "chrome":
+        options = Chrome_options()
+        options.add_argument('lang=' + language)
+        driver= webdriver.Chrome(options=options)
+
+    elif request.config.getoption("--browser") == "firefox":
+        profile = FirefoxProfile()
+        profile.set_preference('intl.accept_languages', language)
+        options = Firefox_options()
+
+        options.profile = profile
+        driver= webdriver.Firefox(options=options)
+
+    else:
+        raise ValueError("Invalid browser")
+
     yield driver
     driver.quit()
+
+
--- test_items.py
+++ test_items.py
@@ -9,7 +9,7 @@ def test_basket_buton_present(browser):
     """If only to check the element is present"""
     browser.get(lnk)
     basket = browser.find_elements(By.CLASS_NAME, "btn-add-to-basket")
-    time.sleep(30)
+    time.sleep(3)
     assert len(basket)>0, "The element is not present"
 
 
@@ -17,7 +17,7 @@ def test_basket_button_work(browser):
     """The element is displayed and enabled"""
     browser.get(lnk)
     basket = browser.find_element(By.CLASS_NAME, "btn-add-to-basket")
-    time.sleep(30)
+    time.sleep(3)
 
     assert basket.is_displayed(), "The element is not displayed"
     print('\nThe element is displayed')
@@ -25,8 +25,8 @@ def test_basket_button_work(browser):
     print('The element is enabled')
 
 
-# pytest --language=es
+# pytest --browser=firefox --language=es
 # pytest --language=en
-# pytest --language=ru
+# pytest --browser=chrome--language=ru
 # pytest --language=de
 # pytest --language=fr
